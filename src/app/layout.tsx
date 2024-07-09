d952644420791738b12373fcf786a40f87a318c2
Boluwatife Oyewande <tifeng9@gmail.com>
2024-07-09 16:06:48 +0100

    touchup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className + ' bg-white'}>
+      <body className={inter.className + ' bg-white pt-[8.25rem]'}>
         <MenuContextProvider>
           <CartContextProvider>
             <NavBar />
